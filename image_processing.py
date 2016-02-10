88026cb64db42ec6ba0d0c791c54436779e90249
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2016-02-10 19:40:28 +0530

    Completed Image Processing class base structure

--- image_processing.py
+++ image_processing.py
@@ -7,22 +7,24 @@
 # sudo pip install pyssim (if used)
 # sudo pip install -U scikit-image (need to install sudo apt-get install python-scipy)
 
-import numpy as np
+#import numpy as np
 import cv2
 import sys
 import os
 from PIL import Image
 from pytesseract import *
-from matplotlib import pyplot as plt
-from skimage.measure import structural_similarity as ssim
-#from skimage.measure import structural_similarity as ssim
-
+#from matplotlib import pyplot as plt
+from subprocess import check_output
 
 class ImageProcessing(object):
 
 	def __init__(self, image):
+
+
 		self.imagePath = image
-		self.imgOpen = Image.open(self.imagePath)
+
+		if self.imageValidation():
+			self.imgOpen = Image.open(self.imagePath)
 
 	def faceDetection(self):
 
@@ -32,13 +34,13 @@ class ImageProcessing(object):
 		img = cv2.imread(self.imagePath)
 		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-		faces = face_cascade.detectMultiScale(gray, 1.2, 3, minSize=(30, 30))
+		faces = face_cascade.detectMultiScale(gray, 1.2, 3, minSize=(20, 20), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
 
 		# Draw a rectangle around the faces
 		for (x, y, w, h) in faces:
 			cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-		#cv2.w('img',img)
+		#cv2.imshow('img',img)
 		imageFilename = os.path.basename(self.imagePath)
 		
 		cv2.imwrite('images/face_detect/'+imageFilename, img)
@@ -62,85 +64,52 @@ class ImageProcessing(object):
 		for color in img_rgb.getdata():
 			colors[color] = colors.get(color, 0) + 1
 
-		#print str(sorted(colors.keys()))
-
-		#img = cv2.imread(self.imagePath)
-		#color = ('b','g','r')
-		#for i,col in enumerate(color):
-			#histr = cv2.calcHist([img],[i],None,[256],[0,256])
-			#plt.plot(histr,color = col)
-			#plt.xlim([0,256])
-		#plt.show()
-		#B G R
-
 		return self.getColorPercentage(colors)
 		
 	def checkDuplicateImage(self, compare_path):
 		
-		# load the images
-		originalImgRead = cv2.imread(self.imagePath)
-
-		# convert the images to grayscale
-		originalGreyScale = cv2.cvtColor(originalImgRead, cv2.COLOR_BGR2GRAY)
-
-		originImageSize = self.imgOpen
+		images_compare = check_output(["python", "ssim", self.imagePath, compare_path+"/*"])
 
 		duplicateImages = []
-		for fname in os.listdir(compare_path):
-    
-			path = os.path.join(compare_path, fname)
-			shortname, extension = os.path.splitext(fname)
-    
-			if os.path.isfile(os.path.join(path)) and fname != "test3-orig.jpg" :
-    	
-				# load the images
-				imagesRead = cv2.imread(compare_path+"/"+fname)
-				imageSize = Image.open(compare_path+"/"+fname)
-
-				# convert the images to grayscale
-				imagesGreyScale = cv2.cvtColor(imagesRead, cv2.COLOR_BGR2GRAY)
-
-				if cmp(originImageSize.size, imageSize.size) == 0:
-					ssimResult = self.compare_images(originalGreyScale, imagesGreyScale)
-					if ssimResult >= 0.99:
-						duplicateImages.append(compare_path+"/"+fname)
+		split_result = images_compare.split(' - ')
+		for image_path in split_result:
+			splitImagePath = image_path.split('\n')
+			if len(splitImagePath) == 2 :
+				duplicateImg = splitImagePath[0].split(':')
+				
+				duplicateImgRatio = float(duplicateImg[1]) * 100
+				if duplicateImgRatio > 45:
+					duplicateImages.append(duplicateImg[0])
 
 		return duplicateImages
-
-	def compare_images(self,imageA, imageB):
-
-		# compute the mean squared error and structural similarity
-		# index for the images
-		ssimResult = ssim(imageA, imageB)
-		return ssimResult 
 
 	def getColorPercentage(self, colors):
 
 		red_color = yellow_color = orange_color = green_color = blue_color = white_color = pink_color = black_color = grey_color = purple_color = brown_color = 0
 
 		for bgr_code in colors.keys():
-			
-			if bgr_code[2] > 150 and  bgr_code[1] < 50 and bgr_code[0] < 50:
+			# BGR RGB
+			if bgr_code[0] > 150 and  bgr_code[1] < 50 and bgr_code[2] < 50:
 				red_color += 1
-			elif bgr_code[2] > 200 and bgr_code[1] > 200 and bgr_code[0] < 100:
+			elif bgr_code[0] > 200 and bgr_code[1] > 200 and bgr_code[2] < 100:
 				yellow_color += 1
-			elif bgr_code[2] > 200 and bgr_code[1] > 100 and bgr_code[1] < 200 and bgr_code[0] <50:
+			elif bgr_code[0] > 200 and bgr_code[1] > 100 and (bgr_code[2] < 200 and bgr_code[2] <50):
 				orange_color += 1
-			elif bgr_code[2] < 50 and bgr_code[1] > 150 and bgr_code[0] < 50:
+			elif bgr_code[0] < 50 and bgr_code[1] > 150 and bgr_code[2] < 50:
 				green_color += 1
-			elif bgr_code[2]< 50 and bgr_code[1] < 50 and bgr_code[0] > 150:
+			elif bgr_code[0]< 50 and bgr_code[1] < 50 and bgr_code[2] > 150:
 				blue_color += 1
-			elif bgr_code[2] > 240 and bgr_code[1] > 240 and bgr_code[0] > 240:
+			elif bgr_code[0] > 240 and bgr_code[1] > 240 and bgr_code[2] > 240:
 				white_color += 1
-			elif bgr_code[2] < 40 and bgr_code[1] < 40 and bgr_code[0] < 40:
+			elif bgr_code[0] < 40 and bgr_code[1] < 40 and bgr_code[2] < 40:
 				black_color += 1
-			elif bgr_code[2] > 150 and bgr_code[1] < 50 and bgr_code[0] > 150:
+			elif bgr_code[0] > 150 and bgr_code[1] < 50 and bgr_code[2] > 150:
 				pink_color += 1
-			elif bgr_code[2] > 40 and bgr_code[2] < 240 and bgr_code[1] > 40 and bgr_code[1] < 240 and bgr_code[0] > 40 and bgr_code[0] < 240:
+			elif (bgr_code[0] > 40 and bgr_code[0] < 240) and (bgr_code[1] > 40 and bgr_code[1] < 240) and (bgr_code[2] > 40 and bgr_code[2] < 240):
 				grey_color += 1
-			elif bgr_code[2] >80 and bgr_code[2] < 120 and bgr_code[1] > 0 and bgr_code[1] < 110 and bgr_code[0] > 180:
+			elif (bgr_code[0] > 80 and bgr_code[0] < 120) and (bgr_code[1] > 0 and bgr_code[1] < 110) and bgr_code[2] > 180:
 				purple_color += 1
-			elif bgr_code[2] > 70 and bgr_code[2] < 120 and bgr_code[1] < 50 and bgr_code[0] < 50:
+			elif (bgr_code[0] > 70 and bgr_code[0] < 120) and bgr_code[1] < 50 and bgr_code[2] < 50:
 				brown_color += 1
 
 
@@ -161,18 +130,32 @@ class ImageProcessing(object):
 
 		return clorsPercentage
 
+	def imageValidation(self):
+		
+		filepath, filename = os.path.split(self.imagePath)
+		shortname, extension = os.path.splitext(filename)
+		extensionlist = ['.jpg', '.png', '.gif', '.jpeg']
+		
+		if extension.lower() in extensionlist:
+			return True
+		else:
+			return False
+
 
 imagePath = sys.argv[1]
 imageProcess = ImageProcessing(imagePath)
-print "Face Detection: ", imageProcess.faceDetection()
-print "\n==================================================================================================\n"
 
-print "Text: ", imageProcess.extract_text()
+if imageProcess.imageValidation():
 
-print "\n==================================================================================================\n"
+	print "Face Detection: ", imageProcess.faceDetection()
+	print "\n==================================================================================================\n"
 
-print "Color Percentage: ", imageProcess.getImageColor()
+	print "Text: ", imageProcess.extract_text()
+	print "\n==================================================================================================\n"
 
-print "\n==================================================================================================\n"
+	print "Color Percentage: ", imageProcess.getImageColor()
+	print "\n==================================================================================================\n"
 
-print "Duplicate Image Path: ", imageProcess.checkDuplicateImage("images/test-images")
+	print "Duplicate Images Path: ", imageProcess.checkDuplicateImage("images/test-images")
+else:
+	print "Invalid Image"
