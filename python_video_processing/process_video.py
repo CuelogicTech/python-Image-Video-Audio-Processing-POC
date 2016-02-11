ceeb53d3414ff193bd345c0f31911cf7a91ab998
pranavn-cuelogic <pranav.naxane@cuelogic.co.in>
2016-02-11 19:24:02 +0530

    - Refactor code - Added the common function for uploading video & audio - Added validation for both audio & video - Added code to divide the audio file into chunks

--- python_video_processing/process_video.py
+++ python_video_processing/process_video.py
@@ -1,5 +1,6 @@
 import cv2
 import sys
+from pydub import AudioSegment
 import moviepy.editor as mp
 import speech_recognition as sr
 import time
@@ -7,16 +8,17 @@ import urlparse
 import os.path
 
 
-class videoProcess(object):
+class processAudioVideo(object):
 
-    def __init__(self, videoPath):
-        self.videoPath = videoPath
-        video_split_path = os.path.splitext(os.path.basename(urlparse.urlsplit(self.videoPath).path))
-        self.videoName = video_split_path[0]
+    def __init__(self, filePath):
+        self.filePath = filePath
+        file_split_path = os.path.splitext(os.path.basename(urlparse.urlsplit(self.filePath).path))
+        self.fileName = file_split_path[0]
 
-    def getVideoLengthAndFps(self):
+    def processVideo(self, mediaType):
 
-        video = cv2.VideoCapture(self.videoPath)
+        video = cv2.VideoCapture(self.filePath)
+        self.fileType = mediaType
 
         # Find OpenCV version
         (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
@@ -24,20 +26,20 @@ class videoProcess(object):
         if int(major_ver) < 3:
             fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
             total_frame_count = video.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
-            fps, length, seconds = self.calculateLengthAndFps(video, total_frame_count, fps)
+            fps, length, seconds = self.calculateVideoLengthAndFps(video, total_frame_count, fps)
 
         else:
             fps = video.get(cv2.CAP_PROP_FPS)
             total_frame_count = video.get(cv2.cv.CAP_PROP_FRAME_COUNT)
-            fps, length, seconds = self.calculateLengthAndFps(video, total_frame_count, fps)
+            fps, length, seconds = self.calculateVideoLengthAndFps(video, total_frame_count, fps)
 
         print "=>Frames per second using: {0}".format(fps)
         print "=>Length of video: {0} minutes".format(length)
-        audio_result = self.convertVideoToAudio(seconds)
-        print "=>Transcription: ", str(audio_result)
+        response = self.processMedia(seconds, self.fileType)
+        print "=>Transcription: ", str(response)
         video.release()
 
-    def calculateLengthAndFps(self, video, total_frame_count, fps):
+    def calculateVideoLengthAndFps(self, video, total_frame_count, fps):
 
         count = 1
         seconds = (total_frame_count / fps)
@@ -51,50 +53,101 @@ class videoProcess(object):
         while is_exist:
             is_exist, frame = video.read()
             if count % 30 == 0:
-                cv2.imwrite('frames/frame_' + self.videoName + '_' + str(count) + '.jpg', frame)
+                cv2.imwrite('frames/frame_' + self.fileName + '_' + str(count) + '.jpg', frame)
             count = count + 1
             cv2.waitKey(1)
 
         return fps, minutes, seconds
 
-    def convertVideoToAudio(self, length):
+    def processAudio(self, mediaType):
+
+        self.fileType = mediaType
+        sound = AudioSegment.from_file(self.filePath)
+        sound.export('audio/' + self.fileName + '.wav', format="wav")
+        self.filePath = 'audio/' + self.fileName + '.wav'
+
+        seconds = (len(sound) / 1000)
+        actual_length = time.strftime("%H:%M:%S", time.gmtime(seconds))
+
+        print "=>Length of audio: {0} minutes".format(actual_length)
+        response = self.processMedia(seconds, self.fileType)
+        print "=>Transcription: ", str(response)
+
+    def processMedia(self, actual_length, mediaType):
         start = 0
         end = 10
         text = ''
-        remaining_length = 0
+        length = int(actual_length)
+        func = self.processVideoChunks if mediaType == 'video' else self.processAudioChunks
 
-        length = int(round(length))
+        # if the length of the video is less than 10 secs.
+        if(length > 10):
+            quotient = divmod(length, 10)
+            quo1 = quotient[0]
+            quo2 = quotient[1]
+            length = quo1 + 1
+
         for i in range(0, length):
 
-            if remaining_length < 10 and remaining_length > 0:
-                if i % remaining_length == 0:
-                    clip = mp.VideoFileClip(self.videoPath).subclip(start, end)
-                    clip.audio.write_audiofile("audio/" + self.videoName + '_' + str(i) + ".wav")
-                    text += str(self.convertAudioToText("audio/" + self.videoName + '_' + str(i) + ".wav"))
+            if quo1 == i and quo2 > 0:
+                end = (start + quo2) - 1
+                text += func(start, end, i)
 
-            elif i % 10 == 0:
-                clip = mp.VideoFileClip(self.videoPath).subclip(start, end)
-                clip.audio.write_audiofile("audio/" + self.videoName + '_' + str(i) + ".wav")
-                text += str(self.convertAudioToText("audio/" + self.videoName + '_' + str(i) + ".wav"))
+            else:
+                text += func(start, end, i)
+                start = end + 1
+                end = end + 10
 
-                start = i + 1
-                end = i + 10
-                remaining_length = length - i
+        return text
 
+    def processAudioChunks(self, start, end, i):
+        clip = mp.AudioFileClip(self.filePath).subclip(start, end)
+        clip.write_audiofile("audio/" + self.fileName + '_' + str(i) + ".wav")
+        text = str(self.convertAudioToText("audio/" + self.fileName + '_' + str(i) + ".wav"))
+        return text
+
+    def processVideoChunks(self, start, end, i):
+        clip = mp.VideoFileClip(self.filePath).subclip(start, end)
+        clip.audio.write_audiofile("audio/" + self.fileName + '_' + str(i) + ".wav")
+        text = str(self.convertAudioToText("audio/" + self.fileName + '_' + str(i) + ".wav"))
         return text
 
     def convertAudioToText(self, audioPath):
         r = sr.Recognizer()
-        with sr.WavFile('audio/audio.wav') as source:  # for now I used hardcoded path, will change later
+        with sr.WavFile(audioPath) as source:  # for now I used hardcoded path, will change later
             audio = r.record(source)  # extract audio data from the file
 
         try:
-            print("Transcription: " + r.recognize_google(audio, show_all=False))  # recognize speech using Google Speech Recognition
-            return r.recognize_google(audio)
-        except LookupError:  # speech is unintelligible
-            print("Could not understand audio")
-            return 'Could not understand audio'
+            # recognize speech using Google Speech Recognition
+            return r.recognize_google(audio) + ' '
+        except:
+            # print("Could not understand audio") # speech is unintelligible
+            return ''
 
-videoPath = sys.argv[1]
-videoObject = videoProcess(videoPath)
-videoObject.getVideoLengthAndFps()
+    def validateExt(self):
+
+        file_path, filename = os.path.split(self.filePath)
+        shortname, extension = os.path.splitext(filename)
+
+        video_extension_list = ['.mp4']
+        audio_extension_list = ['.wav', '.mp3', '.ogg', '.mpeg', '.wma', '.m4a']
+
+        if extension.lower() in video_extension_list:
+            return 'video'
+        elif extension.lower() in audio_extension_list:
+            return 'audio'
+        else:
+            return False
+
+filePath = sys.argv[1]
+fileObject = processAudioVideo(filePath)
+responseObj = fileObject.validateExt()
+
+if str(responseObj) == 'video':
+    fileObject.processVideo('video')
+elif str(responseObj) == 'audio':
+    fileObject.processAudio('audio')
+else:
+    print '\n', '~~' * 40
+    print "Invalid format.. Please provide ('.mp4', '.wav', '.mp3', '.ogg', '.mpeg', '.wma', '.m4a') format file"
+    print '~~' * 40, '\n'
