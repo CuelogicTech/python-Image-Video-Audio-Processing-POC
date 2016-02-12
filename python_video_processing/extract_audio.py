31acc11dbf75949268c49a3b7fcfb5dc0ff41f97
Pranav Naxane <pranav.naxane@cuelogic.co.in>
2016-02-12 15:51:45 +0530

    Remove unwanted code from the folder

--- python_video_processing/extract_audio.py
+++
@@ -1,9 +0,0 @@
-# For extracting audio from video:
-# Install the moviepy first before running this code:
-# In Terminal run the command: (sudo) pip install moviepy
-
-# https://github.com/imageio/imageio-binaries/raw/master/ffmpeg/ffmpeg.linux64
-
-import moviepy.editor as mp
-clip = mp.VideoFileClip("WorldStores.mp4").subclip(0, 5)
-clip.audio.write_audiofile("audio.wav")
